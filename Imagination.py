d6f5056316fd38d3980400b20b0834820f7144bb
smalka2121 <108003309+smalka2121@users.noreply.github.com>
2022-09-29 12:08:41 +0200

    v1

---
+++ Imagination.py
@@ -0,0 +1,11 @@
+
+import sys 
+# Saisie nom et prénom
+Nom_Prénom=input("Veuillez saisir votre nom et votre prénom :")
+print(Nom_Prénom) 
+Age=input("Veuillez saisir votre âge :")
+print(Age)
+Situation=input("Veuillez saisir votre situation :")
+print(Nom_Prénom)
+Perso=input("Voici votre fiche avec vos informations personnelles:")
+print(Nom_Prénom,Age, Situation)
