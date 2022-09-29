e7829eac40364957b91dacf1785a7d7b4bef5067
Abderrahim-ennadir <114389841+Abderrahim-ennadir@users.noreply.github.com>
2022-09-29 12:36:16 +0200

    finalisation et commentaires

--- listev1.py
+++ listev1.py
@@ -1,8 +1,15 @@
 # Script permettant d'encoder des valeurs dans une liste
 # jusqu'à ce que l'utilisateur ne rentre plus de valeurs (ENTER)
 
+#initialisation de la liste
 liste = []
 valeur = "start"
 
-while (valeur != ""):
-    print("Veuillez entrer une valeur :", end = " ")
+#boucle principale
+while (valeur != ""): 
+    print("Veuillez entrer une valeur :", end = " ") #tant que valeur n'est pas nul
+    valeur = input() #incrément de la variable avec la valeur entrée
+    liste.append(valeur) #incrément de la liste avec le contenu de la variable
+
+#affichage de la liste
+print(liste)
