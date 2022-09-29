a012e14f5612e360560c71b3e5632208507ecb18
smalka2121 <108003309+smalka2121@users.noreply.github.com>
2022-09-29 12:32:25 +0200

    Update NombrePremier(modifier).py

--- NombrePremier(modifier).py
+++ NombrePremier(modifier).py
@@ -1,9 +1,9 @@
 
 #afficher les nombres premiers
-min = int(input("Entrez le min : "))
-max = int(input("Entrez le max : "))
-for n in range(min,max + 1):
-   if n > 1:
+min = int(input("Entrez le min : ")) #on choisis le chiffre min de notre intervale
+max = int(input("Entrez le max : ")) #on choisis le chiffre max de notre intervale
+for n in range(min,max + 1): #Pour un chiffre allant de notre variable min à max
+   if n > 1: 
        for i in range(2,n):
            if (n % i) == 0:
                break
